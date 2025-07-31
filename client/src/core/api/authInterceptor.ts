87df14251f41c080aebfbf753f713989c9d2b744
L3me05 <lemueldimarcantonio.elite@gmail.com>
2025-07-31 16:52:40 +0200

    fix: rimosso authInterceptor.ts inutilizzato

--- client/src/core/api/authInterceptor.ts
+++ client/src/core/api/authInterceptor.ts
@@ -1,12 +1,12 @@
-import axios from "axios";
-import { User } from '@auth0/auth0-react';
-
-export const setupAuthInterceptor = (getAccessTokenSilently: () => Promise<string>) => {
-    axios.interceptors.request.use(
-        async (config) => {
-            try {
-                const token = await getAc
-            }
-        }
-    )
-}
+// import axios from "axios";
+// import { User } from '@auth0/auth0-react';
+//
+// export const setupAuthInterceptor = (getAccessTokenSilently: () => Promise<string>) => {
+//     axios.interceptors.request.use(
+//         async (config) => {
+//             try {
+//                 const token = await getAc
+//             }
+//         }
+//     )
+// }
